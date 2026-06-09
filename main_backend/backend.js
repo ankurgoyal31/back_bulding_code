30e4db14cfd77328ef10d3e42e68605a1cf69a82
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-06-09 11:06:44 +0530

    Update backend.js

--- main_backend/backend.js
+++ main_backend/backend.js
@@ -1,48 +1,8 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// dotenv.config();
-// import { connectToDatabase } from './connection.js';
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// const port = 5000;
- 
-// app.get('/health', (req, res) => {
-//     res.status(200).json({ message: 'Server is running!' });
-// }); 
-
-// app.get('/projects_video/:slug', async (req, res) => {
-//     try {
-//     const projectCollection = await connectToDatabase();        
-//     const projects = await projectCollection.findOne({ slug: req.params.slug });
-//     res.status(200).json(projects); 
-//     } catch (error) {
-//     console.error('Error fetching projects:', error);
-//         res.status(500).json({ message: 'Internal Server Error' });
-//     }
-// });
- 
-// app.get('/projects', async (req, res) => {
-//     try {
-//     const projectCollection = await connectToDatabase();        
-//     const projects = await projectCollection.find({}).toArray();
-//     res.status(200).json(projects); 
-//     } catch (error) {
-//     console.error('Error fetching projects:', error);
-//         res.status(500).json({ message: 'Internal Server Error' });
-//     }
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
-// });
-
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectToDatabase } from './connection.js';
-
+import { ObjectId } from 'mongodb';
 dotenv.config();
 
 const app = express();
@@ -100,6 +60,33 @@ app.get('/CSR_data', async (req, res) => {
     }
 });
 
+app.get("/blogs", async (req, res) => {
+    try {
+        const { blog } = await connectToDatabase();
+
+        const blogData = await blog.find({}).toArray();
+
+        res.status(200).json(blogData);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+app.get("/blogs/:id", async (req, res) => {
+    try {
+        const { blog } = await connectToDatabase();
+        const blogId = req.params.id;
+
+        const blogData = await blog.findOne({ _id: new ObjectId(blogId) });
+        res.status(200).json(blogData);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
