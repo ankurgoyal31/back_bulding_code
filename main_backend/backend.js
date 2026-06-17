3713b46533ee5a9d5594922fd55b34fce19d4f2c
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-06-17 14:34:43 +0530

    Update backend.js

--- main_backend/backend.js
+++ main_backend/backend.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { connectToDatabase } from './connection.js';
+import { connectToDatabase, upload } from "./connection.js";
 import { ObjectId } from 'mongodb';
 dotenv.config();
 
@@ -19,7 +19,7 @@ app.get('/health', (req, res) => {
 app.get('/projects_video/:slug', async (req, res) => {
     try {
         const { project } = await connectToDatabase();
-
+ 
         const projects = await project.findOne({
             slug: req.params.slug
         });
@@ -83,6 +83,138 @@ app.get("/blogs/:id", async (req, res) => {
         res.status(200).json(blogData);
 
     } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+app.get("/media", async (req, res) => {
+    try {
+        const { media } = await connectToDatabase();
+        const mediaData = await media.find({}).toArray();
+        res.status(200).json(mediaData);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+app.get("/story", async (req, res) => {
+    try {
+        const { story } = await connectToDatabase();
+        const storyData = await story.find({}).toArray();
+        res.status(200).json(storyData); 
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+app.get("/values", async (req, res) => {
+    try {
+        const { value } = await connectToDatabase();
+        const storyData = await value.find({}).toArray();
+        res.status(200).json(storyData);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
+app.get("/impacts", async (req, res) => {
+    try {
+        const { impact } = await connectToDatabase();
+        const impactData = await impact.find({}).toArray();
+        res.status(200).json(impactData);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
+app.get("/careers", async (req, res) => {
+    try {
+        const { career } = await connectToDatabase();
+        const careerData = await career.find({}).toArray();
+        res.status(200).json(careerData);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+app.post("/apply", upload.single("cv"), async (req, res) => {
+    try {
+        const {apply} = await connectToDatabase();
+
+        const data = {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            experience: req.body.experience,
+            job: req.body.job,
+            post: req.body.post,
+            cv: req.file ? req.file.location : null, // AWS S3 URL
+            createdAt: new Date()
+        };
+
+        const result = await apply.insertOne(data);
+
+        res.status(201).json({
+            success: true,
+            message: "Application submitted successfully",
+            data: result
+        }); 
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+});
+
+
+app.post("/subscribe", upload.single("cv"), async (req, res) => {
+    try {
+        const {contact} = await connectToDatabase();
+
+        const data = {
+             email: req.body.text,
+             message:req.body.message,
+             createdAt: new Date()
+        };
+await contact.insertOne(data)
+        res.status(201).json({
+            success: true,
+            message: "Application submitted successfully",
+        }); 
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+});
+
+app.get("/home_data", async (req, res) => {
+    try {
+        const { home } = await connectToDatabase();
+        const homeData = await home.find({}).toArray();
+        res.status(200).json(homeData);
+    }
+    catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
