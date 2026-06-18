7476d79fc7872cc28010be564d6360b87526c073
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-06-18 14:30:36 +0530

    Uncomment database connection code and add popup collection

--- main_backend/connection.js
+++ main_backend/connection.js
@@ -1,27 +1,3 @@
-// import { MongoClient } from 'mongodb';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const client = new MongoClient(process.env.MONGO_URI);
-
-// let db;
-
-// async function connectToDatabase() {
-//     if (!db) {
-//         console.log("Connecting to database...");
-//         await client.connect();
-//         db = client.db("test");
-//         console.log("Database connected");
-//     }
-//     return {project: db.collection("projects"),blog: db.collection("blogs"),csr: db.collection("csrs"),media: db.collection("media"),values: db.collection("values"),career: db.collection("career"),story: db.collection("ourstories"),value: db.collection("values"),impact: db.collection("impacts"),career: db.collection("careers")};
-// }
-
-// export { connectToDatabase };
-
-
-
-
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import multer from "multer";
@@ -98,6 +74,7 @@ async function connectToDatabase() {
         contact:db.collection("contacts"),
         home:db.collection("homes"),
         apply:db.collection("applied"),
+        popup:db.collection("popups"),
     };
 }
 
